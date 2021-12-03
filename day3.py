ede8f720ccbf29d400e13b69148778cf40338d3f
Bruno Loureiro <matrikss@gmail.com>
2021-12-03 09:53:35 +0000

    Add day 3 part 2

--- day3.py
+++ day3.py
@@ -1,3 +1,39 @@
+def find_most(numbers, position, untie):
+    numbers_count = len(numbers)
+    most = numbers_count / 2
+    ones = 0
+    for number in numbers:
+        ones += int(number[position])
+    if ones > most:
+        return 1
+    if ones == most:
+        return untie
+    else:
+        return 0
+
+
+def find_least(numbers, position, untie):
+    numbers_count = len(numbers)
+    most = numbers_count / 2
+    ones = 0
+    for number in numbers:
+        ones += int(number[position])
+    if ones > most:
+        return 0
+    if ones == most:
+        return untie
+    else:
+        return 1
+
+
+def find_rating(numbers, wanted, index, function):
+    if len(numbers) == 1:
+        return numbers[0]
+    most = function(numbers, index, wanted)
+    filtered = [n for n in numbers if int(n[index]) == most]
+    return find_rating(filtered, wanted, index + 1, function)
+
+
 with open('input3.txt') as f:
     read_data = f.read()
 
@@ -26,5 +62,8 @@ with open('input3.txt') as f:
     gamma = int(gamma, 2)
     epsilon = int(epsilon, 2)
 
+    oxygen = int(find_rating(numbers, 1, 0, find_most), 2)
+    co2 = int(find_rating(numbers, 0, 0, find_least), 2)
+
     print(f'Part 1: {gamma} * {epsilon} = {gamma * epsilon}')
-    # print(f'Part 2: {horizontal_pos} * {depth_part2} = {horizontal_pos * depth_part2}')
+    print(f'Part 2: {oxygen} * {co2} = {oxygen * co2}')
