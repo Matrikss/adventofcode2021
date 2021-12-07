ec554f0abc0f51e47c88745a65e3772c729a7f63
Bruno Loureiro <matrikss@gmail.com>
2021-12-07 19:56:08 +0000

    Add day 6 part 2

--- day6.py
+++ day6.py
@@ -1,25 +1,45 @@
 NEW_TIMER = 8
 RESET_TIMER = 6
 SPAWN = 0
-SIMULATION_END = 80
 
 # input = [3, 4, 3, 1, 2]
+
 input = [3, 3, 5, 1, 1, 3, 4, 2, 3, 4, 3, 1, 1, 3, 3, 1, 5, 4, 4, 1, 4, 1, 1, 1, 3, 3, 2, 3, 3, 4, 2, 5, 1, 4, 1, 2, 2, 4, 2, 5, 1, 2, 2, 1, 1, 1, 1, 4, 5, 4, 3, 1, 4, 4, 4, 5, 1, 1, 4, 3, 4, 2, 1, 1, 1, 1, 5, 2, 1, 4, 2, 4, 2, 5, 5, 5, 3,
          3, 5, 4, 5, 1, 1, 5, 5, 5, 2, 1, 3, 1, 1, 2, 2, 2, 2, 1, 1, 2, 1, 5, 1, 2, 1, 2, 5, 5, 2, 1, 1, 4, 2, 1, 4, 2, 1, 1, 1, 4, 2, 5, 1, 5, 1, 1, 3, 1, 4, 3, 1, 3, 2, 1, 3, 1, 4, 1, 2, 1, 5, 1, 2, 1, 4, 4, 1, 3, 1, 1, 1, 1, 1, 5, 2, 1,
          5, 5, 5, 3, 3, 1, 2, 4, 3, 2, 2, 2, 2, 2, 4, 3, 4, 4, 4, 1, 2, 2, 3, 1, 1, 4, 1, 1, 1, 2, 1, 4, 2, 1, 2, 1, 1, 2, 1, 5, 1, 1, 3, 1, 4, 3, 2, 1, 1, 1, 5, 4, 1, 2, 5, 2, 2, 1, 1, 1, 1, 2, 3, 3, 2, 5, 1, 2, 1, 2, 3, 4, 3, 2, 1, 1, 2,
          4, 3, 3, 1, 1, 2, 5, 1, 3, 3, 4, 2, 3, 1, 2, 1, 4, 3, 2, 2, 1, 1, 2, 1, 4, 2, 4, 1, 4, 1, 4, 4, 1, 4, 4, 5, 4, 1, 1, 1, 3, 1, 1, 1, 4, 3, 5, 1, 1, 1, 3, 4, 1, 1, 4, 3, 1, 4, 1, 1, 5, 1, 2, 2, 5, 5, 2, 1, 5]
 
-day = 0
-while day < SIMULATION_END:
-    spawns = []
-    for i in range(0, len(input)):
-        if input[i] == SPAWN:
-            spawns.append(NEW_TIMER)
-            input[i] = RESET_TIMER
-        else:
-            input[i] -= 1
-    input.extend(spawns)
-    day += 1
+cache = {}
 
-print(f'Part 1: {len(input)}')
-print(f'Part 2: {2}')
+
+def calc_stage(stage, end, now):
+    if (stage, now) in cache:
+        return cache[(stage, now)]
+
+    if now >= end:
+        return 1
+
+    if stage == SPAWN:
+        cache[(NEW_TIMER, now + 1)] = calc_stage(NEW_TIMER, end, now + 1)
+        cache[(RESET_TIMER, now + 1)] = calc_stage(RESET_TIMER, end, now + 1)
+        return cache[(NEW_TIMER, now + 1)] + cache[(RESET_TIMER, now + 1)]
+    else:
+        cache[(stage - 1, now + 1)] = calc_stage(stage - 1, end, now + 1)
+        return cache[(stage - 1, now + 1)]
+
+
+total1 = 0
+for i in input:
+    if (i, 0) not in cache:
+        cache[(i, 0)] = calc_stage(i, 80, 0)
+    total1 += cache[(i, 0)]
+
+cache = {}
+total2 = 0
+for i in input:
+    if (i, 0) not in cache:
+        cache[(i, 0)] = calc_stage(i, 256, 0)
+    total2 += cache[(i, 0)]
+
+print(f'Part 1: {total1}')
+print(f'Part 2: {total2}')
