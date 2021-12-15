6637f9cecb20f44b57aa04bf2e6be6dc65cae6ac
Bruno Loureiro <matrikss@gmail.com>
2021-12-15 19:00:10 +0000

    Reduce memory usage

--- day14.py
+++ day14.py
@@ -1,15 +1,15 @@
 import math
-import re
-import collections
+
+counters = dict()
 
 
 def hail_mary(pairs: str, count):
     if count == 10:
-        return pairs
-    ins = insertions[pairs]
-    one = f'{pairs[0]}{ins}'
-    two = f'{ins}{pairs[1]}'
-    return hail_mary(one, count + 1) + hail_mary(two, count + 1)[1:]
+        return
+    next = insertions[pairs]
+    counters[next[0][1]] += 1
+    hail_mary(next[0], count + 1)
+    hail_mary(next[1], count + 1)
 
 
 with open('input/input14.txt') as f:
@@ -18,35 +18,25 @@ with open('input/input14.txt') as f:
     raw_lines = read_data.split('\n')
     template = raw_lines[0]
     insertions = dict()
-    regexes = dict()
+
     for i in range(2, len(raw_lines)):
         rline = raw_lines[i]
         broken = rline.split()
-        insertions[broken[0]] = broken[2]
-        regexes[broken[0]] = re.compile(f'(?={broken[0]})')
+        insertions[broken[0]] = [f'{broken[0][0]}{broken[2]}', f'{broken[2]}{broken[0][1]}']
+        if broken[0][0] not in counters:
+            counters[broken[0][0]] = 0
 
-    cache = dict()
+    for l in list(template):
+        counters[l] += 1
 
-    tst = hail_mary(template[0:2], 0)
-    cache[template[0:2]] = tst[1:]
-    for i in range(1, len(template) - 1):
-        print(i)
+    for i in range(0, len(template) - 1):
+        # print(i)
         pair = template[i:i + 2]
-        if pair in cache:
-            tst += cache[pair]
-            continue
-        res = hail_mary(pair, 0)[1:]
-        cache[pair] = res
-        tst += res
-
-    template = tst
-
-    letters = list(template)
-    frequency = collections.Counter(letters)
+        hail_mary(pair, 0)
 
     min_f = math.inf
     max_f = 0
-    for k, v in frequency.items():
+    for k, v in counters.items():
         min_f = min(min_f, v)
         max_f = max(max_f, v)
 
