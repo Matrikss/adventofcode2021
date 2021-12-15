586bcd8445b4fda4762ebc778f23f62b4522fd60
Bruno Loureiro <matrikss@gmail.com>
2021-12-15 19:33:16 +0000

    Add day 4 part 2

--- day14.py
+++ day14.py
@@ -1,15 +1,22 @@
 import math
+from collections import Counter
 
-counters = dict()
 
-
-def hail_mary(pairs: str, count):
-    if count == 10:
-        return
+def get_insertion_count(pairs: str, count, cache):
+    if (pairs, count) in cache:
+        return cache[(pairs, count)]
+    if count == 40:
+        return Counter()
     next = insertions[pairs]
-    counters[next[0][1]] += 1
-    hail_mary(next[0], count + 1)
-    hail_mary(next[1], count + 1)
+    counter = Counter()
+    counter[next[0][1]] = 1
+    a = get_insertion_count(next[0], count + 1, cache)
+    cache[(next[0], count + 1)] = a.copy()
+    counter.update(a)
+    b = get_insertion_count(next[1], count + 1, cache)
+    cache[(next[1], count + 1)] = b.copy()
+    counter.update(b)
+    return counter
 
 
 with open('input/input14.txt') as f:
@@ -18,27 +25,27 @@ with open('input/input14.txt') as f:
     raw_lines = read_data.split('\n')
     template = raw_lines[0]
     insertions = dict()
-
+    counters = dict()
     for i in range(2, len(raw_lines)):
         rline = raw_lines[i]
         broken = rline.split()
         insertions[broken[0]] = [f'{broken[0][0]}{broken[2]}', f'{broken[2]}{broken[0][1]}']
         if broken[0][0] not in counters:
             counters[broken[0][0]] = 0
+    result = Counter(counters)
+    caching = result.copy()
+    for i in range(0, len(template) - 1):
+        pair = template[i:i + 2]
+        result.update(get_insertion_count(pair, 0, caching))
 
     for l in list(template):
-        counters[l] += 1
-
-    for i in range(0, len(template) - 1):
-        # print(i)
-        pair = template[i:i + 2]
-        hail_mary(pair, 0)
+        result[l] += 1
 
     min_f = math.inf
     max_f = 0
-    for k, v in counters.items():
+    for k, v in result.items():
         min_f = min(min_f, v)
         max_f = max(max_f, v)
 
-    print(f'Part 1: {max_f - min_f}')
-    print(f'Part 2: {2}')
+    print(f'Part 1: 3009')  # just change the 40 to a 10 to get this number
+    print(f'Part 2: {max_f - min_f}')
